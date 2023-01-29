6f9d73a43e093d2602ccb202b678330bb541de03
Felicity Lipscomb <2443084l@student.gla.ac.uk>
2023-01-29 21:15:00 +0000

    Added loopRate function

--- skeleton_code/flight_controller.ino
+++ skeleton_code/flight_controller.ino
@@ -21,27 +21,38 @@ void calibrateAttitude() {
 
 // MAIN LOOP
 // -----------------------------------------------------
-// Reads IMU data from
+
+// Reads IMU data from BNO055 IMU sensor using Adafruit library
+// TO BE DONE
 void getIMUdata() {
   
 }
-
+// Reads Altitude data from SharpIR GP2Y0A710K distance sensor
+// TO BE DONE
 void getAltitudeData() {
 
 }
-
+// Normalizes desired control values to appropriate values.
+// Also creates roll_passthru, pitch_passthru, and yaw_passthru variables
+// for controlMixer()
+// TO BE DONE
 void getDesState() {
 
 }
-
-void controlSystem() {
+//Computes control commands based on state error (angle) in cascaded scheme
+// Different control system will be used depending on the current state
+// TO BE COPIED
+void controlSystem(int mode) {
   
 }
-
-void controlMixer() {
+// Mixing commands to motor and servo
+// Different control mixing will depend on the current state
+// TO BE DONE
+void controlMixer(int mode) {
 
 }
-
+//Computes control commands based on state error (angle) in cascaded scheme
+// TO BE DONE
 void scaleCommands() {
   
 }
@@ -49,7 +60,7 @@ void scaleCommands() {
 void commandOutput() {
 
 }
-
+//Get values for every channel from the radio
 void getCommands() {
 
 }
@@ -57,7 +68,14 @@ void getCommands() {
 void failSafe() {
 
 }
-
-void loopRate(int frequency) {
-
+//Regulate main loop rate to specified frequency in Hz
+// COPIED
+void loopRate(int freq) {
+  float invFreq = 1.0/freq*1000000.0;
+  unsigned long checker = micros();
+  
+  //Sit in loop until appropriate time has passed
+  while (invFreq > (checker - current_time)) {
+    checker = micros();
+  }
 }
