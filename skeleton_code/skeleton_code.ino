d67cdd279fd58dbfaa7519b160fbb0eb310e6cba
Felicity Lipscomb <2443084l@student.gla.ac.uk>
2023-01-29 14:50:29 +0000

    Created Skeleton Code file with credits

---
+++ skeleton_code/skeleton_code.ino
@@ -0,0 +1,49 @@
+
+
+
+
+// X Wing Flight Controller 
+// Authors: 
+
+// Credits:
+// Flight controller skeleton code borrowed from:
+// dRhemFlight Flight Controller - 
+//             Nicholas Rehm
+//             Department of Aerospace Engineering
+//             University of Maryland
+//             College Park 20742
+//             https://github.com/nickrehm/dRehmFlight
+// -----------------------------------------------------
+// Understanding of BNO055 IMU Sensor from:
+// Adafruit BNO055 Absolute Orientation Sensor - 
+//             Kevin Townsend
+//             Adafruit
+//             Last Accessed: 29/01/23
+//             https://learn.adafruit.com/adafruit-bno055-absolute-orientation-sensor/arduino-code
+// -----------------------------------------------------
+// Understanding of GP2Y0A710K distance sensor:
+//             Makerguides
+//             Last Accessed: 29/01/23
+//             https://www.makerguides.com/sharp-gp2y0a710k0f-ir-distance-sensor-arduino-tutorial/
+// -----------------------------------------------------
+
+// Required Libraries:
+#include <Adafruit_BNO055.h> // For the BNO055 IMU sensor
+#include <SharpIR.h> // For the SharpIR GP2Y0A710K distance sensor
+// Pin Selection:
+// NOTE: PINS PUT HERE AS PLACEHOLDERS, WILL BE CHANGED IN FINAL VERSION
+// Radio:
+const int ch1pin, ch2pin, ch3pin; 
+//BNO055
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
