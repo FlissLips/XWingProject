288b6766933ae14a9e15f510e612cd0cbfd3a9ac
Felicity Lipscomb <2443084l@student.gla.ac.uk>
2023-02-27 17:12:15 +0000

    New comms with softwareserial

--- RadioComms/RadioComms.ino
+++ RadioComms/RadioComms.ino
@@ -1,4 +1,5 @@
 #include "sbus.h"
+#include <stdio.h>
 
 // Reciever Test
 // This is to read from the SBUS reciever (FrSky L9R)
@@ -7,26 +8,35 @@
 
 
 
+#include <SoftwareSerial.h>
 // SBUS object on hardware serial port 1 
 bfs::SbusRx l9r(1);
 bfs::SbusData data;
+SoftwareSerial mySerial (10, 11);
 void setup() {
   Serial.begin(115200);
+  // Serial.println("Spinning up...");
   while (!Serial) {}
   //begin SBUS Communication
   l9r.Begin();
 
+
 }
 
 void loop() {
+  // Start the SBUS Communication
+   Serial.begin(115200);
    // Get the Recieved data
+  char buffer[data.NUM_CH];
   if(l9r.Read()){
     data = l9r.data();
     for (int8_t i = 0; i < data.NUM_CH; i++) {
-      Serial.print(data.ch[i]);
-      Serial.print("\t");
+      buffer[i] = data.ch[i];
+      mySerial.print(data.ch[i]);        
+      mySerial.print("\t");  
     }
-
+    // End SBUS Communication
+    
   }
 
 }
