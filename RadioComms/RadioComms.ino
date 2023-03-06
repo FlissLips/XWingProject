a9546357dfbfc5f7ff8e4b9e5ccbfd81479dd8b8
Felicity Lipscomb <2443084l@student.gla.ac.uk>
2023-03-06 16:04:17 +0000

    Changed to work with SBUS Comms and Teensy

--- RadioComms/RadioComms.ino
+++ RadioComms/RadioComms.ino
@@ -1,42 +1,34 @@
+
 #include "sbus.h"
-#include <stdio.h>
 
-// Reciever Test
-// This is to read from the SBUS reciever (FrSky L9R)
-// And print it to the monitor.
-// Thanks to library Sbus https://github.com/bolderflight/sbus 
-
-
-
-#include <SoftwareSerial.h>
-// SBUS object on hardware serial port 1 
-bfs::SbusRx l9r(1);
+/* SBUS object, reading SBUS */
+bfs::SbusRx sbus_rx(&Serial2);
+/* SBUS data */
 bfs::SbusData data;
-SoftwareSerial mySerial (10, 11);
-void setup() {
-  Serial.begin(115200);
-  // Serial.println("Spinning up...");
-  while (!Serial) {}
-  //begin SBUS Communication
-  l9r.Begin();
 
+void setup() {
+  /* Serial to display data */
+  Serial.begin(115200);
+  // while (!Serial) {}
+  /* Begin the SBUS communication */
+  sbus_rx.Begin();
 
 }
 
-void loop() {
-  // Start the SBUS Communication
-   Serial.begin(115200);
-   // Get the Recieved data
-  char buffer[data.NUM_CH];
-  if(l9r.Read()){
-    data = l9r.data();
+void loop () {
+ if (sbus_rx.Read()) {
+    /* Grab the received data */
+    data = sbus_rx.data();
+    /* Display the received data */
     for (int8_t i = 0; i < data.NUM_CH; i++) {
-      buffer[i] = data.ch[i];
-      mySerial.print(data.ch[i]);        
-      mySerial.print("\t");  
+      Serial.print(data.ch[i]);
+      Serial.print("\t");
     }
-    // End SBUS Communication
-    
+          Serial.println();
+    /* Display lost frames and failsafe data */
+    // Serial.print(data.lost_frame);
+    // Serial.print("\t");
+    // Serial.println(data.failsafe);
   }
 
 }
