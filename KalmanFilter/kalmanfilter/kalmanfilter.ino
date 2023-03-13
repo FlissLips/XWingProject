5e8ee9f6ae0d11b2b55e7cdb2c0e4e5c03ce885f
Felicity Lipscomb <2443084l@student.gla.ac.uk>
2023-03-13 10:10:50 +0000

    Added barometer and distance sensor to the KF

--- KalmanFilter/kalmanfilter/kalmanfilter.ino
+++ KalmanFilter/kalmanfilter/kalmanfilter.ino
@@ -1,5 +1,16 @@
+
+#include "Arduino.h"
+#include <Wire.h> // I2C Communication
 #include <BasicLinearAlgebra.h>
 #include <ElementStorage.h>
+// #include <HP20x_dev.h>
+#include <Adafruit_BNO055.h> // For the BNO055 IMU sensor
+#include <barometer_dubbed.h>
+
+#define C1 1125.0
+#define C2 137500.0
+#define ACCEL_SCALE_FACTOR 9.8
+#define pinNo 17
 
 class KF {
   public: 
@@ -68,8 +79,8 @@ void KF::update_step(BLA::Matrix<1,3> observation, BLA::Matrix<1> measurement, B
   BLA::Matrix<3> kalman_gain = KF::kalman_gain(KF::covariance, observation, measurement_covariance);
   BLA::Matrix<3> new_state = KF::state_update(KF::state, kalman_gain, observation, measurement);
   BLA::Matrix<3,3> new_covariance = KF::covariance_update(KF::covariance,  kalman_gain, observation, measurement_covariance);
-  KF::state = new_state;
-  KF::covariance = new_covariance;    
+  state = new_state;
+  covariance = new_covariance;    
 }
 
 
@@ -78,8 +89,8 @@ void KF::prediction_step(float dt)
   BLA::Matrix<3,3> state_transition = KF::state_transition(dt);
   BLA::Matrix<3> new_state = KF::state_extrapolation(KF::state, state_transition);
   BLA::Matrix<3,3> new_covariance = KF::covariance_extrapolation(covariance, state_transition, KF::process_noise);
-  KF::state = new_state;
-  KF::covariance = new_covariance;
+  state = new_state;
+  covariance = new_covariance;
 
 }
 
@@ -105,11 +116,79 @@ BLA::Matrix<3,3> KF::covariance_extrapolation(BLA::Matrix<3, 3> covariance, BLA:
 }
 
 
-
+Adafruit_BNO055 bno(55, 0x28, &Wire);
+unsigned long lastUpdate;
+KF filter;
 void setup(){
+// Setup IMU
+// initializing the sensor
 
+  Serial.println("Currently Testing IMU...");
+  if (!bno.begin())
+  {
+    // if it cannot detect the BNO055
+    Serial.println("BNO055 initialization unsuccessful");
+    Serial.println("Check Wiring... or maybe the I2C address..?");
+    while (1)
+    {
+    }
+  }
+  Serial.println("IMU Sucessful... \n Currently Testing Grove Hi-Accuracy Barometer...");
+  barometer_begin();
+  Serial.println("Barometer successful... \n Testing Distance Sensor");
+  pinMode(pinNo,INPUT);
+
+  lastUpdate = millis();
+
+  
+  
 }
 
 void loop(){
+  float dt = (millis()-lastUpdate)/1E3;
+  lastUpdate = millis();
+
+  BLA::Matrix<1,3> observation_matrix_position = {0,0,1};
+  BLA::Matrix<1> measurement_noise_barometer = {100};
+  BLA::Matrix<1> measurment_noise_dist_snr = {10};
+  BLA::Matrix<1,3> observation_matrix_acc = {1,0,0};
+  BLA::Matrix<1> measurement_noise_acc = {5};
+
+  unsigned long altitude = barometer_read_altitude()/100;
+  BLA::Matrix<1> measurement_barometer = {altitude};
+
+  filter.prediction_step(dt);
+  filter.update_step(observation_matrix_position, measurement_barometer, measurement_noise_barometer);
+ 
+  sensors_event_t linearAccelData;
+  bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+  double AccZ=linearAccelData.acceleration.z/ACCEL_SCALE_FACTOR;
+  BLA::Matrix<1> measurement_acc = {AccZ};  
+
+  filter.update_step(observation_matrix_acc, measurement_acc, measurement_noise_acc);
+
+  uint16_t distance_sensor = analogRead(pinNo);
+  BLA::Matrix<1> measurement_dist_snr = {distance_sensor};
+  filter.update_step(observation_matrix_position, measurement_dist_snr, measurment_noise_dist_snr);
+
+
+  Serial.print("Original Accelerometer: ");
+  Serial.println(AccZ);
+  Serial.print("Original Barometer: ");
+  Serial.println(altitude);
+  Serial.print("Original Distance Sensor: ");
+  Serial.println(distance_sensor);
+  Serial.print("Filtered Altitude: ");
+  Serial.println(filter.state(2));
+  Serial.print("Covariance: ");
+  Serial.println(filter.covariance(2,2));
+
+  delay(500);
+  
+  unsigned char ret = barometer_available();
+  if(ret == OK_HP20X_DEV)
+    Serial.println("bar ok");
+  else
+    Serial.println("bar bad");
 
 }
