123b0f235a9367c2f2de5f4e966a95a42e4aef3d
Felicity Lipscomb <2443084l@student.gla.ac.uk>
2023-03-16 13:25:53 +0000

    Added distance sensor init function

--- flightController/functions.ino
+++ flightController/functions.ino
@@ -49,6 +49,7 @@ void IMUinit()
      * Don't worry about how this works.
      */
     // initializing the sensor
+    Serial.println("Testing the BNO055 IMU Sensor...");
     if (!bno.begin())
     {
         // if it cannot detect the BNO055
@@ -58,6 +59,57 @@ void IMUinit()
         {
         }
     }
+
+}
+void barometerUnit()
+{
+  // DESCRIPTION: Initialise Barometer
+  /*
+  * This is added by us, and is not tested as much. 
+  * Also had to use our own library, since the original library.
+  * didn't work on Teensy. 
+  */
+ if (!HP20x.begin()){
+  Serial.println("Barometer does not work...");
+  Serial.println("Check Wiring... or maybe the I2C address..?"); 
+  }
+}
+
+void ultrasonicUnit()
+{
+  // DESCRIPTION: Initalise ultrasonic distance sensor
+  /*
+  * This is added by us, and is not tested as much.
+  *
+  */
+  Serial.println("Testing the ultrasonic distance Sensor...");
+  int BUF_SIZE = 30;
+  int C1 = 1125;
+  int C2 = 137500.0;
+  uint16_t buffer[BUF_SIZE] = {0};
+  int counter = 0;
+
+  uint16_t reading = analogRead(distanceSensorPin);
+  for (int i = 1; i < BUF_SIZE; i++)
+    buffer[i] = buffer[i - 1];
+  buffer[0] = reading;
+
+  int avg = 0;
+  for (int i = 0; i < BUF_SIZE; i++)
+    avg += buffer[i];
+  avg /= BUF_SIZE;
+
+  double readingMillivolts = 1000.0 * avg * (5.0 / 1024.0);
+  double distanceCentimetres = 1.0 / ((readingMillivolts - C1) / C2);
+  delay(1);
+  if (counter++ >= 1000)
+  {
+    counter = 0;
+    Serial.print("Measuring current (unfiltered) distance as: ");
+    Serial.print(distanceCentimetres);
+    Serial.println("m. \n");
+    Serial.println("Please make sure this is correct.");
+  }
 }
 
 // CHANGED FOR OUR IMU
